83b22e4403bdd4458f5241dcab9dcf2f01604e37
afong <adam.y.w.fong@gmail.com>
2023-09-20 19:13:43 -0700

    updates to search portion of js functions

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,7 @@ var resultsContainer = $('#results-container');
 var lyricsEl = $('#lyrics-container');
 
 //api keys for easy replacement if limits are exceeded
-var keyMusMatch = 'a2175728fd0b1091b79cae95435a1216'+'buffer'; 
+var keyMusMatch = 'a2175728fd0b1091b79cae95435a1216' + 'buffer'; 
 var keyYT = 'AIzaSyBRuDvIUX8S79zEXDUNkaqpftfEY7jjaNQ'+'buffer';
 var lastSearch;
 
@@ -13,30 +13,34 @@ var lastSearch;
 
 
 //Pseudocode for expected required functionality
+function init() {
 
-// function handleFormSubmit() {
-//     var query = $('#text-submission').val();
-//     fetch('http://api.musixmatch.com/ws/1.1/track.search?q=' + query + '&s_track_rating=desc&apikey=' + keyMusMatch)
-//         .then(function(response){
-//             if (!response.ok) {
-//                 throw response.json();
-//             }
-//             return response.json();
-//         })
-//         .then(function(data) {
-//             diplayResults(data);
-//         });
-// }
+}
 
-// function diplayResults(data) {
-//     console.log(data);
-//     lastSearch = data;
-//     for (var i=0; i < data.track_list.length; i++) {
-//         var resultEl = $('<div>');
-//         resultEl.text(data.track_list[i].track_name);
-//         $('#results-container').append(resultEl);
-//     }
-// }
+function handleFormSubmit(event) {
+    event.preventDefault();
+    var query = userInput.children().eq(0).val();
+    fetch('http://api.musixmatch.com/ws/1.1/track.search?q=' + query + '&page_size=4&s_track_rating=desc&apikey=' + keyMusMatch)
+        .then(function(response){
+            if (!response.ok) {
+                throw response.json();
+            }
+            return response.json();
+        })
+        .then(function(data) {
+            displayResults(data);
+        });
+}
+
+function displayResults(data) {
+    console.log(data);
+    lastSearch = data;
+    for (var i=0; i < data.track_list.length; i++) {
+        var resultEl = $("<li class='song-option'>");
+        resultEl.text(data.track_list[i].track_name);
+        $('#results-container').append(resultEl);
+    }
+}
 
 // function handleResultsClick(event) {
 //     event.preventDefault()
@@ -77,5 +81,6 @@ var lastSearch;
 //         });
 //}
 
+init();
 userInput.on('submit', handleFormSubmit);
-resultsContainer.on('click','.btn-play', handleResultsClick)
+// resultsContainer.on('click','.btn-play', handleResultsClick)
