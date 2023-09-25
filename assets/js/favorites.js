3b46e56c7f6d9b1669681bc57f358eaaec92dde0
afong <adam.y.w.fong@gmail.com>
2023-09-25 14:17:43 -0700

    Added back button and remove buttons to favorites page

--- assets/js/favorites.js
+++ assets/js/favorites.js
@@ -1,32 +1,31 @@
 var favoritesList;
-var favoritesContainer = document.getElementById("favorites-list")
+var favoritesContainer = document.getElementById("favorites-list");
+var backButton = document.getElementById("btn-back");
 
 function init() {
     favoritesList = localStorage.getItem("favorites");
     if (favoritesList) {
         favoritesList = JSON.parse(favoritesList);
-        console.log (favoritesList);
     } else {
         favoritesList = [];
     }
 }
 
 function displayFavorites() {
-    var favoritesContainer = document.getElementById("favorites-list");
     favoritesContainer.innerHTML = "";
-
     if (favoritesList.length > 0) {
         for (var i=0; i<favoritesList.length;i++) {
             var listItem = document.createElement("li");
             var video = document.createElement("iframe");
             var button = document.createElement("button");
-            video.setAttribute("class", "has-ratio");
+            video.setAttribute("class", "has-ratio video");
             video.setAttribute("src", "https://www.youtube.com/embed/" + favoritesList[i].videoID);
             video.setAttribute("title", favoritesList[i].videoTitle);
             video.setAttribute("frameborder", 0);
+            video.setAttribute("data-index", i );
             video.setAttribute("allow", "accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share");
             button.setAttribute("class","button is-danger btn-remove");
-            button.textContent("X");
+            button.textContent = "X";
             listItem.append(video);
             listItem.append(button);
             favoritesContainer.append(listItem);
@@ -36,7 +35,15 @@ function displayFavorites() {
     }
 }
 
-window.addEventListener("click", )
+favoritesContainer.addEventListener("click", function(event) {
+    var clicked = event.target;
+    if (clicked.matches(".btn-remove")) {
+        var videoIndex = parseInt(clicked.previousSibling.dataset.index);
+        favoritesList.splice(videoIndex,1);
+        localStorage.setItem("favorites", JSON.stringify(favoritesList));
+        displayFavorites();
+    }
+})
 
 // Call the init function when the favorites.html page loads
 window.addEventListener("load", function () {
